afe0bf8f1962d44ebe43fe3927bd1d8ffe7c16d9
Ola Horg Jacobsen <olah.jac@gmail.com>
2025-05-05 23:21:09 +0200

    Delete .env.cmake

--- .env.cmake
+++
@@ -1,9 +0,0 @@
-set(GLFW_PATH C:/Users/kjeks/Documents/Libraries/glfw-3.4)
-set(GLM_PATH C:/Users/kjeks/Documents/Libraries/glm)
-set(VULKAN_SDK_PATH  C:/VulkanSDK/1.4.309.0)
-
-# Set MINGW_PATH if using mingwBuild.bat and not VisualStudio20XX
-set(MINGW_PATH "C:/Users/kjeks/Documents/Libraries/mingw64")
-
-# Optional set TINYOBJ_PATH to target specific version, otherwise defaults to external/tinyobjloader
-# set(TINYOBJ_PATH X:/dev/Libraries/tinyobjloader)
